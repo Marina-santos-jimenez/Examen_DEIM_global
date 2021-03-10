9ffb3475d48ca583b2b85236f5aad79596f3d94d
AlexisFarfan <jumpytunechi@gmail.com>
2021-03-10 19:10:13 +0100

    material. Movimiento cubo, cámara

---
+++ New Unity Project (1)/Assets/scrip/CubeMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private float moveSpeed = 3f;
+    private bool inMarginMoveX = true;
+    private bool inMarginMoveY = true;
+    void Start()
+    {
+        speed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverCube();
+    }
+    void MoverCube()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+        float desplZ = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+
+        if (posX < 20f && posX > -20f || posX < -20f && desplX > 0 || posX > 20f && desplX < 0)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed * desplX);
+        }
+
+        if (posZ < 20f && posZ > -20f || posZ < -20f && desplZ > 0 || posZ > 20f && desplZ < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/scrip/camara_Look.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara_Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform playerPosition;
+    [SerializeField] float smoothVelocity = 0.1f;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerPosition);
+        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y +4, playerPosition.position.z +5);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+    }
+}
